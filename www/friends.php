414455ed73f09a56a0ba8a8c9a3dfed69567ffab
Suryanarayanan Balasubramanian <surya.mit@gmail.com>
2012-09-22 18:54:07 -0400

    A sample working Fb authentication code

--- www/friends.php
+++ www/friends.php
@@ -1,29 +1,73 @@
+<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>demo</title>
+  </head>
+<body>
+Finally !!
 <?php
 
 // Get these from http://developers.facebook.com
-$api_key = '422128541181076';
-$secret  = '16a32a398469d4003043a75116747cde';
-
 include_once '../facebook/php/facebook.php';
+$app_id = '422128541181076';
+$secret  = '16a32a398469d4003043a75116747cde';
+$facebook = new Facebook(array(
+  'appId'  => $app_id,
+  'secret' => $secret,
+  'cookie' => false,
+));
 
-$facebook = new Facebook($api_key, $secret);
-$user = $facebook->require_login();
+//$session = $facebook->getSession();
+$friends = array();
+$userData = null;
+
+ {
+    try {
+         $uid = $facebook->getUser();      
+         $me = $facebook->api('/me');
+    }catch (FacebookApiException $e) {
+   }
+}
+
+if ($me) {
+	$logoutUrl = $facebook->getLogoutUrl();
+	} else {
+	$loginUrl = $facebook->getLoginUrl();
+}
 
 ?>
 
 <h1>Friends list demo</h1>
-Hello <fb:name uid='<?php echo $user; ?>' useyou='false' possessive='true' />! <br>
-Your id : <?php echo $user; ?>.
+<?php if ($me): ?>
+ <?php echo "Welcome, ".$me['first_name']. ".<br />"; ?>
+	<a href="<?php echo $logoutUrl; ?>">
+	      <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+	    </a>
+	    <?php else: ?>
+	      <a href="<?php echo $loginUrl; ?>">
+	        <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
+	 </a>
+<?php endif ?>
 
-You have several friends:<br>
+
 <?
+/*
 $friends = $facebook->api_client->friends_get();
+*/
 ?>
 
 <ul>
 <?
+/*
 foreach($friends as $friend){
 	echo "<li><fb:name uid=\"$friend\" useyou=\"false\"></li>";	
 }
+*/
 ?>
 </ul>
+
+</body>
+
+</html>
