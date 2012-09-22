2a38a21872b526e05d68261261b6b9d88ab2df9f
Suryanarayanan Balasubramanian <surya.mit@gmail.com>
2012-09-22 16:33:41 -0400

    Initial layout and fb php sdk

---
+++ www/friends.php
@@ -0,0 +1,29 @@
+<?php
+
+// Get these from http://developers.facebook.com
+$api_key = '422128541181076';
+$secret  = '16a32a398469d4003043a75116747cde';
+
+include_once './facebook-platform/php/facebook.php';
+
+$facebook = new Facebook($api_key, $secret);
+$user = $facebook->require_login();
+
+?>
+
+<h1>Friends list demo</h1>
+Hello <fb:name uid='<?php echo $user; ?>' useyou='false' possessive='true' />! <br>
+Your id : <?php echo $user; ?>.
+
+You have several friends:<br>
+<?
+$friends = $facebook->api_client->friends_get();
+?>
+
+<ul>
+<?
+foreach($friends as $friend){
+	echo "<li><fb:name uid=\"$friend\" useyou=\"false\"></li>";	
+}
+?>
+</ul>
